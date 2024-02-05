19c834104dc9b855508f96ec053d15297a1e1136
Callum Kloos <callumkloos@gmail.com>
2024-02-05 17:15:25 +0000

    custom hook for handling loader vs. filter data

---
+++ app/features/explore/hooks/use-initial-data.ts
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+import { useTypedFetcher, useTypedLoaderData } from "remix-typedjson";
+
+export const useRouteData = <L>() => {
+  const loaderData = useTypedLoaderData<L>();
+  const [data, setData] = useState(loaderData);
+  const fetcher = useTypedFetcher<L>();
+  useEffect(() => {
+    if (fetcher.data !== undefined && fetcher.data !== data) {
+      setData(fetcher.data);
+    }
+  }, [fetcher, data]);
+	return {
+		data,
+		fetcher
+	}
+}
