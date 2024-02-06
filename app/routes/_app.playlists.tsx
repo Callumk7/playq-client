b8ece92278a6a6d502d33e85cedbd3a038d5cdb6
Callum Kloos <callumkloos@gmail.com>
2024-02-06 10:04:45 +0000

    remove playlist nav

--- app/routes/_app.playlists.tsx
+++ app/routes/_app.playlists.tsx
@@ -1,36 +1,8 @@
-import { Link, Outlet } from "@remix-run/react";
+import { Outlet } from "@remix-run/react";
 export default function PlaylistsRoute() {
   return (
     <div>
-      <PlaylistNav />
       <Outlet />
     </div>
-  );
-}
-
-const playlistLinks = [
-  {
-    to: "/playlists/browse",
-    name: "Browse",
-  },
-  {
-    to: "/playlists/friends",
-    name: "Friends",
-  },
-];
-
-function PlaylistNav() {
-  return (
-    <nav className="flex gap-8">
-      {playlistLinks.map((link) => (
-        <Link
-          key={link.name}
-          to={link.to}
-          className="text-foreground/60 hover:text-foreground transition-colors ease-in-out"
-        >
-          {link.name}
-        </Link>
-      ))}
-    </nav>
   );
 }
