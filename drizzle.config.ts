36b47dd9c53a851c6798e221fed74969fc416765
Callum Kloos <callumkloos@gmail.com>
2024-08-11 15:47:37 +0100

    move to new database

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,6 +2,9 @@ import type { Config } from "drizzle-kit";
 
 export default {
 	schema: "./db/schema/*",
-	driver: "pg",
+	dialect: "postgresql",
 	out: "./drizzle",
+	dbCredentials: {
+		url: process.env.C_DATABASE_URL!
+	}
 } satisfies Config;
